adc1d3287ba230428141b50180508e7348ee5b03
Alex <aliaksandrlashkov@gmail.com>
2021-09-21 19:19:53 +0300

    fix1

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-
 python -m pip install --user --upgrade pip
-python -m install setuptools
+python -m pip install setuptools
 #python -m pip install -q kfp --upgrade --user
 python -m pip install --user --upgrade kfp
 
